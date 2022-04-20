4790059ca0de1e84075612ea573c71dc80d19cfd
herendjb <99762300+herendjb@users.noreply.github.com>
2022-04-20 11:55:55 -0400

    Update urls.py

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -3,9 +3,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('josh/', views.index, name='index'),
-    path('index2/', views.index2, name='index2'),
+    path('index/', views.index, name='index'),
     path('ins/', views.instructor, name='instructor'),
     path('stu/', views.student, name='student'),
     path('dept/', views.department, name='department'),
+    path('f1/', views.F1, name='F1'),
+    path('f2/', views.F2, name='F2'),
+    path('f3/', views.F3, name='F3'),
+    path('f4/', views.F4, name='F4'),
+    path('f5/', views.F5, name='F5'),
+    path('f6/', views.F6, name='F6'),
 ]
