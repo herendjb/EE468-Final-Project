ba7f4d45f24f09c8f14c0c6ae3561f3a11ee1e8d
herendjb <99762300+herendjb@users.noreply.github.com>
2022-04-22 10:04:46 -0400

    Update urls.py

--- mysite1/urls.py
+++ mysite1/urls.py
@@ -4,13 +4,21 @@ from . import views
 
 urlpatterns = [
     path('index/', views.index, name='index'),
-    path('ins/', views.instructor, name='instructor'),
-    path('stu/', views.student, name='student'),
     path('dept/', views.department, name='department'),
-    path('f1/', views.F1, name='F1'),
+    path('f11/', views.F1_1, name='F1_1'),
+    path('f12/', views.F1_2, name='F1_2'),
+    path('f13/', views.F1_3, name='F1_3'),
     path('f2/', views.F2, name='F2'),
     path('f3/', views.F3, name='F3'),
     path('f4/', views.F4, name='F4'),
     path('f5/', views.F5, name='F5'),
     path('f6/', views.F6, name='F6'),
+	##
+    path('form/', views.get_main, name='get_main'),
+    path('fail/', views.fail, name='fail'),
+    path('success/', views.success, name='success'),
+    path('admin/', views.get_admin, name='get_admin'),
+    path('f1/', views.get_f1, name='get_f1'),
+    path('pro/', views.get_professor, name='get_professor'),
+    path('stu/', views.get_student, name='get_student'),
 ]
