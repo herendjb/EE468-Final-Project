7e5ed104402dd14730f652cfd9939f7e5a8ef74f
herendjb <99762300+herendjb@users.noreply.github.com>
2022-04-26 14:47:45 -0400

    Update views.py

--- mysite1/views.py
+++ mysite1/views.py
@@ -2,12 +2,64 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from .forms import NameForm
+from django.contrib.auth import authenticate
 
 def index(request):
     return HttpResponse("Hello, world.")
 
-def fail(request):
-    return HttpResponse("Failure of form")
+def login_admin(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/admin/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'login_adm.html', {'form': form})
+
+def login_professor(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/pro/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'login_pro.html', {'form': form})
+
+def login_student(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = NameForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('/stu/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = NameForm()
+
+    return render(request, 'login_stu.html', {'form': form})
 
 def success(request):
     return HttpResponse("Success of form")
@@ -382,6 +434,9 @@ def department(request): #http://localhost:8000/dept/
 	mycursor.close()
 	mydb.close()
 	return HttpResponse(s)
+
+
+
 
 
 
